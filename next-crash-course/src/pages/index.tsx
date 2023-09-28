67206d31b02ac9876de45ed7e1d0266d03ce5a4d
MOwais <owace.ahmed@gmail.com>
2023-09-28 16:27:32 -0700

    articles component

--- next-crash-course/src/pages/index.tsx
+++ next-crash-course/src/pages/index.tsx
@@ -1,13 +1,27 @@
 import Head from "next/head"; //for custom titles, meta tags, descriptions etc
+import ArticleList from "../components/ArticleList";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <>
       <Head>
         <title>WebDev Newz</title>
         <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to Next</h1>
+      <ArticleList articles={articles} />
     </>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
