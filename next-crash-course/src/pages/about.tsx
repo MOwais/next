c47513271d8b8a01da1c07c08e590662a23e534e
MOwais <owace.ahmed@gmail.com>
2023-09-28 15:48:38 -0700

    initial nav bar

---
+++ next-crash-course/src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function about() {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      about
+    </div>
+  );
+}
--- next-crash-course/src/pages/index.tsx
+++ next-crash-course/src/pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from 'next/head';
-
+import Head from "next/head"; //for custom titles, meta tags, descriptions etc
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>WebDev Newz</title>
-        <meta name="keywords" content="web development, programming"/>
+        <meta name="keywords" content="web development, programming" />
       </Head>
-      <h1>Welcome to next</h1>
+      <h1>Welcome to Next</h1>
     </>
-  )
+  );
 }
