2163550efa9913fd822a87a6ff9b41c8475ad6c5
MOwais <owace.ahmed@gmail.com>
2023-09-28 20:28:01 -0700

    api with local data

--- next-crash-course/src/pages/index.tsx
+++ next-crash-course/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"; //for custom titles, meta tags, descriptions etc
 import ArticleList from "../components/ArticleList";
+import { server } from "../../config";
 
 interface Article {
   articles: {
@@ -11,21 +12,11 @@ interface Article {
 }
 
 function Home({ articles }: Article) {
-  return (
-    <>
-      <Head>
-        <title>WebDev Newz</title>
-        <meta name="keywords" content="web development, programming" />
-      </Head>
-      <ArticleList articles={articles} />
-    </>
-  );
+  return <ArticleList articles={articles} />;
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
-  );
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
