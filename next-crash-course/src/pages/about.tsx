aa69265f2ee2991b9ffa6678f5a6e09d7d4482cc
MOwais <owace.ahmed@gmail.com>
2023-09-28 20:28:10 -0700

    meta component

--- next-crash-course/src/pages/about.tsx
+++ next-crash-course/src/pages/about.tsx
@@ -1,12 +1,11 @@
 import Head from "next/head";
+import Meta from "@/components/Meta";
 
 export default function about() {
   return (
     <div>
-      <Head>
-        <title>About</title>
-      </Head>
-      about
+      <Meta title="About" />
+      <h1>About</h1>
     </div>
   );
 }
